a46262296f1e5663b33836e0246310722e5da9f9
Matthew Veety <mveety@gmail.com>
2012-10-04 03:12:58 -0400

    worked on netkey a bit

--- tools/netkey/main.go
+++ tools/netkey/main.go
@@ -5,7 +5,22 @@ import (
 	"bufio"
 	"fmt"
 	"crypto/md5"
+	"os"
+	"strings"
 )
 
 func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	fmt.Printf("Server: ")	
+	str,err := stdin.ReadString('\n')
+	str = strings.Trim(" \n\r\t")
+	cn, err := net.Dial("tcp",str)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	r := bufio.NewReader(cn)
+	fmt.Printf("User: ")
+	str, err = stdin.ReadString('\n')
+	str = strings.Trim(" \n\t\r")
 	
